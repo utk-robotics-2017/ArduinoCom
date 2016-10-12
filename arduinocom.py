6090da315017fec7e965fa510ea287fc8c4f973f
thisguyisototallyben <thisguyistotallyben@gmail.com>
2016-10-12 19:02:44 -0400

    Modified arduinocom.py

--- arduinocom.py
+++ arduinocom.py
@@ -1,32 +1,42 @@
-import serial
+from PyCmdMessenger import PyCmdMessenger
 
-class pi_to_arduino:
-    def __init__(self, port):
-        self.usb = serial.Serial(
-                   port = port,
-                   baudrate = 115200,
-                   parity = serial.PARITY_ODD,
-                   stopbits = serial.STOPBITS_TWO,
-                   bytesize = serial.SEVENBITS
-                   )
-        self.usb.isOpen()
+# configure_arduino
+# get name of board grabdevice in core?
+# make sure it exists
+# grab config and do messages (command map / messenger)
+# initialize arduino and commands
 
-    def send(self, command):
-        self.usb.write(command)
+devname = input("Enter devname: ")
 
-    def read(self):
-        output = ''
-        while self.usb.inWaiting() > 0:
-            print("received")
-            output += self.usb.read(1)
-        if output != '':
-            return out;
-        else:
-            return "No response"
+arduino = PyCmdMessenger.ArduinoBoard("/dev/" + devname, baud_rate=115200)
 
+# setup
+commands = [["lol",""],
+            commands_config = arduino['commands']
 
-if __name__ == "__main__":
-    test = pi_to_arduino("/dev/mega")
+            commands = [None] * len(commands_config)
+            commands[0] = ["kAcknowledge", "i"]
+            commands[1] = ["kError", "i"]
+            commands[2] = ["kUnknown", ""]
+            commands[3] = ["kSetLed", "?"]
+            commands[4] = ["kPing", ""]
+            commands[5] = ["kPingResult", "i"]
+            commands[6] = ["kPong", ""]
 
-    test.send("ping")
-    print(test.read())
+            self.command_map[devname] = {}
+            self.command_map[devname][0] = "kAcknowledge"
+            self.command_map[devname][1] = "kError"
+            self.command_map[devname][2] = "kUnknown"
+            self.command_map[devname][3] = "kSetLed"
+            self.command_map[devname][4] = "kPing"
+            self.command_map[devname][5] = "kPingResult"
+            self.command_map[devname][6] = "kPong"
+            ["kUnknown", ""],
+            ["what am i doing?", "?"]]
+c = PyCmdMessenger.CmdMessenger(arduino, commands)
+
+c.send("kUnknown")
+
+msg = c.receive();
+
+print(msg)
