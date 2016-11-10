8ab7d86167f5cec58364cd0aff8262b9d96c8d71
Ben Klein <robobenklein@gmail.com>
2016-11-09 20:36:42 -0500

    Working on curses version of CLI

--- ArduinoCom.py
+++ ArduinoCom.py
@@ -1,5 +1,65 @@
 #!/usr/bin/env python3
 
 import os
+import log
+
+try:
+    import curses
+except ImportError as err:
+    log.error("Could not import curses, is it installed?")
+    log.trace(err)
 
 import PyCmdMessenger.PyCmdMessenger
+
+log.DEBUG_LEVEL = 3
+
+
+def interface_wrapper(terminal_screen):
+    terminal_screen.clear()
+
+    application_interface = interface(terminal_screen)
+
+    application_interface.start_loop()
+
+    terminal_screen.refresh()
+    terminal_screen.getkey()
+
+
+class interface:
+    env_windows = {}
+
+    def __init__(self, terminal_screen):
+        terminal_screen.clear()
+        self.create_main_ui(terminal_screen)
+
+    def create_main_ui(self, terminal_screen):
+        terminal_screen.resize(curses.LINES - 20, curses.COLS)
+        terminal_screen.addstr("Loading ArduinoCom...")
+
+        # Make a Title/Status window at the top:
+        terminal_screen.mvwin(2, 0)
+        terminal_screen.refresh()
+        self.env_windows["ui_statusbar"] = curses.newwin(1, curses.COLS, 0, 0)
+        self.env_windows["ui_statusbar"].addstr("ArduinoCom Main Console")
+        self.env_windows["ui_statusbar"].refresh()
+        terminal_screen.refresh()
+
+        # Make a pad for the console in/out to the arduino.
+        self.env_windows["console_pad"] = curses.newpad(curses.LINES - 20, curses.COLS)
+        # Move the console below the status bar
+        self.env_windows["console_pad_coords"] = 2, 0, curses.LINES - 20, curses.COLS
+        self.env_windows["console_pad_scroll"] = 0
+        self.env_windows["console_pad"].addstr(log.info("Pad Initialized."))
+        # self.env_windows["console_pad"].refresh(self.env_windows["console_pad_scroll"], 0, *self.env_windows["console_pad_coords"])
+
+    def start_loop(self):
+        pass
+        # This will be filled later
+
+if __name__ == "__main__":
+    log.info("ArduinoCom CLI loading...")
+
+    log.info("Starting environment...")
+    curses.wrapper(interface_wrapper)
+
+    # End of program.
---
+++ log.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+import sys
+import traceback
+import datetime
+
+import logging
+
+logging.basicConfig(format='\r[%(asctime)s] [%(levelname)s]: %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG
+                    )
+
+# Light Blue
+logging.addLevelName(logging.INFO, "\033[1;94m%s\033[1;0m" % logging.getLevelName(logging.INFO))
+# Yellow
+logging.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+# Red
+logging.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
+# Inverted Red
+logging.addLevelName(logging.CRITICAL, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.CRITICAL))
+
+DEBUG_APPLICATION = False
+DEBUG_LEVEL = 0
+LOG_TRACES = False
+
+
+class bcolors:
+    PURPLE_H = '\033[95m'  # High Purple
+    BLUE = '\033[34m'
+    SUCCESS = '\033[32m'  # Norm Green
+    DEBUG = '\033[90m'  # Grey
+    INFO = '\033[37m'  # # Norm White
+    WARNING = '\033[93m'  # High Yellow
+    CRITICAL = '\033[91m'  # High Red
+    ENDC = '\033[0m'  # Reset
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def debug(message):
+    if DEBUG_APPLICATION:
+        logging.debug(message)
+    return wrap(bcolors.DEBUG + "[DEBUG] " + message)
+
+
+def debug1(message):
+    if DEBUG_LEVEL >= 1:
+        # print(bcolors.DEBUG + "[DEBUG1] " + message + bcolors.ENDC)
+        logging.debug(message)
+
+
+def debug2(message):
+    if DEBUG_LEVEL >= 2:
+        # print(bcolors.DEBUG + "[DEBUG2] " + message + bcolors.ENDC)
+        logging.debug(message)
+
+
+def debug3(message):
+    if DEBUG_LEVEL >= 3:
+        # print(bcolors.DEBUG + "[DEBUG3] " + message + bcolors.ENDC)
+        logging.debug(message)
+
+
+# Multi-line debugging output
+# If the message includes a \n, you should use this
+def trace(message):
+    if DEBUG_APPLICATION and LOG_TRACES:
+        # print(bcolors.DEBUG + "[TRACE] BEGIN >>> " + bcolors.ENDC)
+        for line in message.split("\n"):
+            print(bcolors.DEBUG + "[TRACE] " + line + bcolors.ENDC)
+            logging.debug(line)
+        # print(bcolors.DEBUG + "[TRACE] END <<<" + bcolors.ENDC)
+
+
+def info(message):
+    logging.info(message)
+    return wrap(bcolors.INFO + "[INFO] " + message)
+
+
+def warn(message):
+    logging.warning(message)
+    return wrap(bcolors.WARNING + "[WARN] " + message)
+
+
+def warning(message):
+    # Just an alias
+    warn(message)
+
+
+def error(message):
+    logging.error(message)
+    return wrap(bcolors.CRITICAL + "[ERROR] " + message)
+
+
+def fatal(message):
+    logging.critical(message)
+    return wrap(bcolors.CRITICAL + "[FATAL] " + message)
+
+
+def gettimestamp():
+    return '[{:%Y-%m-%d_%H:%M:%S}]'.format(datetime.datetime.now())
+
+
+def wrap(message):
+    return gettimestamp() + message + bcolors.ENDC
+
+if __name__ == "__main__":
+    logging.debug("Debug info")
+    logging.info("Info info, with info")
+    logging.warning("Warning info")
+    logging.error("Error info")
+    logging.critical("Critical info")
+
+    logging.info("Timestamp: " + gettimestamp())
