99214e5a66c5a6c81ce6751f9b647ecaa59aef42
Anthony Stewart <astewa44@vols.utk.edu>
2017-02-16 01:21:29 +0000

    Added decoding of intpacking

--- appendages/electronic_component_detector.py
+++ appendages/electronic_component_detector.py
@@ -18,7 +18,7 @@ class ElectronicComponentDetector:
                 pad = 9
             if len(args) == 2:
                 try:
-                    pad = int(arsg[1])
+                    pad = int(args[1])
                 except ValueError:
                     help(name)
                     return
@@ -30,8 +30,17 @@ class ElectronicComponentDetector:
                 help(name)
                 return
 
-            val = self.s.get_appendage(name).decode(pad=str(pad))
-            print("{}: {}".format(name, val))
+            val = self.s.get_appendage(name).decode(pad=str(pad))[0]
+            code = []
+            for i in range(5):
+                code.append(val >> (i * 3) & 7)
+            print("{0:s}: raw: {1:d}, raw_b: {2:s}, {3:s} , decoded: {4:s}".format(
+                name,
+                val,
+                "".join(str(val >> (15 - i) & 1) for i in range(16)),
+                "{} ".format(val >> 15 & 1) +
+                    " ".join("{:03b}".format(i) for i in reversed(code)),
+                "".join(str(i) for i in code)))
 
         else:
             help(name)
